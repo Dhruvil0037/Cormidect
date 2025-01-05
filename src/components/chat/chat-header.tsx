dd1454322a63cb845bfa6c57bc8a4d6738b035d8
Dhruvil0037 <dhruvildhamecha2003@gmail.com>
2025-01-05 19:07:44 +0530

    REMOVING MOBILE VIEW

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -1,5 +1,4 @@
 import { Hash } from "lucide-react";
-import MobileToggle from "../mobile-toggle";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -16,7 +15,6 @@ export const ChatHeader = ({
 }: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-b-2 border-neutral-200 dark:border-purple-800 hover:bg-purple-700/10 dark:hover:bg-zinc-700/50 transition">
-      <MobileToggle serverId={serverId} />
       <div className="flex items-center">
         {type === "channel" && (
           <Hash className="w-5 h-5 group-hover:text-accent-foreground dark:group-hover:text-accent-foreground transition mr-2" />
